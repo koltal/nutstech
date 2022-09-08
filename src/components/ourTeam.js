b64b887f0d1b92991da64097877f03fc93398736
koltal <nibrasabubakar@gmail.com>
2022-09-08 21:51:32 +0100

    team portfolio profile

--- src/components/ourTeam.js
+++ src/components/ourTeam.js
@@ -1,6 +1,11 @@
 import taj from './taj.jpg';
 import musty from './musty.jpeg';
 import mubarak from './mubarak.jpeg';
+import sulaiman from './sulaiman.jpeg';
+import nibras from './nibras.jpeg';
+import sadiq from './sadiq.jpeg';
+import olu from './olu.png';
+
 const Teams = () => {
     return ( 
         <div >
@@ -23,7 +28,7 @@ const Teams = () => {
                     <div className="Team-Card">
                         <center>
                         <div>
-                            <img className='Team-img' src={taj} alt="display pic" />
+                            <img className='Team-img' src={sulaiman} alt="display pic" />
                         </div>
                         <div>
                             <span className='Team-Member-Name'>Sulaiman Aminu Barkindo</span><br></br>
@@ -49,6 +54,39 @@ const Teams = () => {
                         </div>
                         <div>
                             <span className='Team-Member-Name'>Mubarak Ibrahim</span><br></br>
+                            <span className='Team-Member-Role'>Frontend Developer</span>
+                        </div>
+                        </center>
+                    </div>
+                    <div className="Team-Card">
+                        <center>
+                        <div>
+                            <img className='Team-img' src={olu} alt="display pic" />
+                        </div>
+                        <div>
+                            <span className='Team-Member-Name'>Oluwaseun Oluyemo</span><br></br>
+                            <span className='Team-Member-Role'>Frontend Developer</span>
+                        </div>
+                        </center>
+                    </div>
+                    <div className="Team-Card">
+                        <center>
+                        <div>
+                            <img className='Team-img' src={sadiq} alt="display pic" />
+                        </div>
+                        <div>
+                            <span className='Team-Member-Name'>Abubakar Sadiq Ismail</span><br></br>
+                            <span className='Team-Member-Role'>Full Stack Developer</span>
+                        </div>
+                        </center>
+                    </div>
+                    <div className="Team-Card">
+                        <center>
+                        <div>
+                            <img className='Team-img' src={nibras} alt="display pic" />
+                        </div>
+                        <div>
+                            <span className='Team-Member-Name'>Muhammad Nibras</span><br></br>
                             <span className='Team-Member-Role'>Frontend Developer</span>
                         </div>
                         </center>
