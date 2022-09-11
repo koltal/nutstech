2d7c524dfc8da1c1a93d7487088acc2eb10d2b56
koltal <nibrasabubakar@gmail.com>
2022-09-11 11:06:15 +0100

    products done

--- src/components/products_detail.js
+++ src/components/products_detail.js
@@ -1,4 +1,6 @@
 import HRMS from './HRMS_Product.png';
+import SRMS from './SRMS.png';
+import BIME from './BIME.png';
 import pics from './background1.png';
 const ProductDetails = () => {
     return ( 
@@ -20,24 +22,24 @@ const ProductDetails = () => {
             <div className="Products-Detail-Container" >
                
                 <div>
-                    <h2>Hospital Record Management System (HRMS)</h2>
+                    <h2>My School Repo</h2>
                     <p>Tertiary Institutions across the country have trusted FlexiSAF to support their quest to deliver quality education to millions of Nigerian students while ensuring smooth running  of their institutions.
                         
                     </p>
                 </div>
                 <div>
-                    <img src={HRMS} width='100%'/>
+                    <img src={SRMS} width='100%'/>
                 </div>
                 
             </div>
 
             <div className="Products-Detail-Container" >
             <div>
-                    <img src={HRMS} width='100%'/>
+                    <img src={BIME} width='100%'/>
                 </div>
                
                 <div>
-                    <h2>Hospital Record Management System (HRMS)</h2>
+                    <h2>BIME</h2>
                     <p>Tertiary Institutions across the country have trusted FlexiSAF to support their quest to deliver quality education to millions of Nigerian students while ensuring smooth running  of their institutions.
                         
                     </p>
