15235333cd84416a100c3835c72f64d5b5e8c42f
koltal <nibrasabubakar@gmail.com>
2022-09-13 15:21:08 +0100

    footer fixed

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import  TwitterIcon from '@mui/icons-material/Twitter';
+import  InstagramIcon from '@mui/icons-material/Instagram';
+import  FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 const Footer = () => {
     return ( 
         <div className="Overall-Footer-Container">
@@ -20,8 +24,10 @@ const Footer = () => {
                         </div>
                         <div>
                              <li>Home</li>
-                             <Link to='about'>About</Link><br></br>
-                             <Link to='products'>Product</Link>
+                             <li>About</li>
+                             <li>Products</li>
+                             {/* <Link to='about'>About</Link><br></br>
+                             <Link to='products'>Product</Link> */}
                             <li>Contact</li>
                             
                         </div>
@@ -36,16 +42,22 @@ const Footer = () => {
                             <li>MySchoolRepo</li>
                             <li>Get Bime</li>
                             <li>HRMS</li>
-                            <li>Trackers</li>
+                           
                         </div>
                     </div>
                 </div>
                 <div>
                     <div><h3>Legal</h3></div>
                     <div>
-                        <p>2022 Nutscoder Products</p>
+                        <p>Tems and conditions apply</p>
                         
-                        <p>Give us a call on <strong>+2348140409672</strong></p>
+                        
+                    </div>
+                    <div>
+                        <TwitterIcon></TwitterIcon>
+                        <InstagramIcon></InstagramIcon>
+                        <FacebookIcon></FacebookIcon>
+                        <LinkedInIcon></LinkedInIcon>
                     </div>
                 </div>
             </div>
